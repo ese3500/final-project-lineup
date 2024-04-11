6c622bf39f7fe73fd49fc2642444726218346acd
ZweTun <144613156+ZweTun@users.noreply.github.com>
2024-04-11 17:07:38 -0400

    Add files via upload

--- motorControl.c
+++ motorControl.c
@@ -23,43 +23,42 @@ void Intialize() {
 		cli(); 
 		
 		
-	//--------------------------------------------------Acclerometer--------------------------//	
-		// Setup for ADC (10bit = 0-1023)
-		// Clear power reduction bit for ADC
-		PRR0 &= ~(1 << PRADC);
-
-		// Select Vref
-		ADMUX |= (1 << REFS0);
-
-
-		// Set the ADC clock div by 128
-		// 16M/128=125kHz
-		ADCSRA |= (1 << ADPS0);
-		ADCSRA |= (1 << ADPS1);
-		ADCSRA |= (1 << ADPS2);
-
-		// Select Channel ADC0 (pin C0)
-		ADMUX &= ~(1 << MUX0);
-		ADMUX &= ~(1 << MUX1);
-		ADMUX &= ~(1 << MUX2);
-		ADMUX &= ~(1 << MUX3);
-
-		ADCSRA |= (1 << ADATE);   // Autotriggering of ADC
-
-		// Free running mode ADTS[2:0] = 000
-		ADCSRB &= ~(1 << ADTS0);
-		ADCSRB &= ~(1 << ADTS1);
-		ADCSRB &= ~(1 << ADTS2);
-
-		// Disable digital input buffer on ADC pin
-		DIDR0 |= (1 << ADC0D);
-		
-		// Enable ADC
-		ADCSRA |= (1 << ADEN);
-
-		// Start conversion
-		ADCSRA |= (1 << ADSC);
-		
+// 	//--------------------------------------------------Acclerometer--------------------------//	
+// 		// Setup for ADC (10bit = 0-1023)
+// 		// Clear power reduction bit for ADC
+// 		PRR0 &= ~(1 << PRADC);
+// 
+// 		// Select Vref
+// 		ADMUX |= (1 << REFS0);
+// 
+// 
+// 		// Set the ADC clock div by 128
+// 		// 16M/128=125kHz
+// 		ADCSRA |= (1 << ADPS0);
+// 		ADCSRA |= (1 << ADPS1);
+// 		ADCSRA |= (1 << ADPS2);
+// 
+// 		// Select Channel ADC0 (pin C0)
+// 		ADMUX &= ~(1 << MUX0);
+// 		ADMUX &= ~(1 << MUX1);
+// 		ADMUX &= ~(1 << MUX2);
+// 		ADMUX &= ~(1 << MUX3);
+// 
+// 		ADCSRA |= (1 << ADATE);   // Autotriggering of ADC
+// 
+// 		// Free running mode ADTS[2:0] = 000
+// 		ADCSRB &= ~(1 << ADTS0);
+// 		ADCSRB &= ~(1 << ADTS1);
+// 		ADCSRB &= ~(1 << ADTS2);
+// 
+// 		// Disable digital input buffer on ADC pin
+// 		DIDR0 |= (1 << ADC0D);
+// 		
+// 		// Enable ADC
+// 		ADCSRA |= (1 << ADEN);
+// 
+// 		// Start conversion
+// 		ADCSRA |= (1 << ADSC);
 		
 		
 		
@@ -92,9 +91,9 @@ void Intialize() {
 
 void updateVelocity(double currSpeed) {
 	if (abs(currSpeed) < consVel - 1) {
-		OCR0B =  (3.222 * currSpeed) + 1;  // Sets duty cycle,
+		OCR0B =  (3.222 * currSpeed) + 1;  // Sets duty cycle based on speed, less speed = more duty cycle 
 	}  else {
-		OCR0B = 40;
+		OCR0B = 40; //Stops motor 
 	}
 }
 
@@ -106,13 +105,12 @@ int main(void)
 	updateVelocity(9); 
     while (1) 
     {
-				int currentAdcRead = ADC;
-				char rawAdcStringBuffer[10];                    // Buffer to hold the converted number
-				itoa(currentAdcRead, rawAdcStringBuffer, 10);   // Convert integer to string
-				UART_putstring("Raw ADC Read:\t");
-				UART_putstring(rawAdcStringBuffer);
-				UART_putstring( "\r\n");
-				
+// 				int currentAdcRead = ADC;
+// 				char rawAdcStringBuffer[10];                    // Buffer to hold the converted number
+// 				itoa(currentAdcRead, rawAdcStringBuffer, 10);   // Convert integer to string
+// 				UART_putstring("Raw ADC Read:\t");
+// 				UART_putstring(rawAdcStringBuffer);
+// 				UART_putstring( "\r\n"				
 				//OCR0B = (0.039 * ADC);   // Sets duty cycle,
 		 
     }
