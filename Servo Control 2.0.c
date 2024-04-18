9fbe3ec8ff8886e241c5dbdfc4e633488d0b9b68
tunw134 <157830794+tunw134@users.noreply.github.com>
2024-04-18 16:44:22 -0400

    Add files via upload

---
+++ Servo Control 2.0.c
@@ -0,0 +1,58 @@
+/*
+ * Servo Control.c
+ *
+ * Created: 4/9/2024 2:25:39 PM
+ * Author : willi
+ */ 
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+#define LEFT 20
+#define RIGHT 26
+
+int timesteps = 0;
+
+void Initialize() {
+	cli();
+	DDRB |= (1 << DDB1);      // Set PB1 to be an output pin
+
+	// Timer1, prescaler of 1
+	//TCCR1B |= (1 << CS10);
+	//TCCR1B |= (1 << CS11);
+	TCCR1B |= (1 << CS12);
+
+	// Timer1, Phase Correct PWM (mode 1)
+	TCCR1A |= (1 << WGM10);
+	
+	//CLear on Compare
+	TCCR1A |= (1 << COM1A1);
+	TCCR1A &= ~(1 << COM1A0);
+	
+	OCR1A = 24; //Duty Cycle
+	
+	//Time Based Interrupt
+	TIMSK1 |= (1 << OCIE1B);
+	OCR1B = 65535;
+	sei();
+}
+
+ISR(TIMER1_COMPB_vect){
+	//if(timesteps > 100){
+		//timesteps = 0;
+		//OCR1A = LEFT;
+	//} else if (timesteps == 50) {
+		//OCR1A = RIGHT;
+		//timesteps++;
+	//} else {
+		//timesteps++;
+	//}
+}
+
+
+
+int main(void) {
+	Initialize();
+	while (1)
+	;
+}
